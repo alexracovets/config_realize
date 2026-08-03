1ae3e64b5aee7fa3d974e5809e2413b0620a3b49
Oleksandr Rakovets <alexracovets@gmail.com>
2026-08-03 02:00:44 -0700

    added logo text

--- src/ui/components/atomic/molecules/ConfigurationTools/LogoUploadedFilesSection/LogoUploadedFilesSection.tsx
+++ src/ui/components/atomic/molecules/ConfigurationTools/LogoUploadedFilesSection/LogoUploadedFilesSection.tsx
@@ -3,12 +3,18 @@
 import { DefaultBrandLogoPlaceholder } from '@molecules/ConfigurationTools/DefaultBrandLogoPlaceholder';
 import { LogoListRow } from '@molecules/ConfigurationTools/LogoListRow';
 import type { logoUploadedFilesSectionPropsType } from '@types';
-import { Flex, Text } from '@atoms';
+import { Flex, Grid, SvgIcon, Text } from '@atoms';
 import { CONFIGURATOR_UPLOADED_FILES_LABEL } from '@constants';
 const LogoUploadedFilesSection = ({ userLogos, onEdit, onDelete }: logoUploadedFilesSectionPropsType) => {
   return (
     <Flex className="flex-col gap-3 max-xl:gap-2.5 items-start w-full">
       <Text className="text-[14px] leading-[15px] max-xl:text-[11px] max-xl:leading-3 text-gray">{CONFIGURATOR_UPLOADED_FILES_LABEL}</Text>
+      <Grid className="grid-cols-[auto_1fr] gap-2.5 max-xl:gap-2 items-center px-3 max-xl:px-2.5 p-2 max-xl:py-1.5 rounded-[4px] bg-primary w-full">
+        <SvgIcon name="info" className="max-xl:size-3.25" />
+        <Text className="text-[12px] max-xl:text-[10px] text-gray">
+          Trascina i loghi davanti, dietro o sulle maniche a seconda di dove li vorresti posizionati.
+        </Text>
+      </Grid>
       <Flex className="flex-col gap-5 max-xl:gap-4 items-start w-full" asChild>
         <ul>
           <li className="w-full">
